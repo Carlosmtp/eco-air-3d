2cf9dab0be7959161bee15312e11975e1232d056
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2024-08-28 23:36:39 -0500

    Add Firebase configuration for web application

---
+++ firebase.config.js
@@ -0,0 +1,35 @@
+/** * @file firebase.config.js
+ * @description This file is responsible for configuring and initializing Firebase services for the web application. It sets up the Firebase app instance, authentication, and Firestore database connections.
+ * @date Created: 28/08/2024
+ * @date Last Modified: 28/08/2024
+ * @author Carlos Mauricio Tovar Parra
+ *         carlos.mauricio.tovar@correounivalle.edu.co
+ * @note Ensure to keep the Firebase configuration secure and do not expose sensitive keys in a public repository.
+*/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth
+const auth = getAuth(app);
+
+// Intialize Firestore
+const db = getFirestore(app);
+
+// Export the auth and db objects
+export { auth, db };
