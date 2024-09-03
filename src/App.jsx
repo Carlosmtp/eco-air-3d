783dbb3c047c0856578c5742b67c9b86b2ccea30
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2024-09-03 10:51:02 -0500

    routes with react-router-dom defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import './App.css'
-import Login from './components/login/Login';
+/**
+ * @file App.jsx
+ * @description The main entry file for the React application that imports styles and the root `App` component to be rendered.
+ */
 
+import './App.css'
+import Login from './pages/login/Login';
+
+/**
+ * @component App
+ * @description The root component of the React application that serves as the entry point and houses the main components of the application.
+ * @returns {JSX.Element} The main application container that includes the Login component.
+ * @example
+ * // Example usage:
+ * <App />
+ */
 function App() {
   return (
     <div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
+/**
+ * @file main.jsx
+ * @description The main entry file for the React application that sets up routing and renders the application into the DOM.
+ * @note This file is responsible for initializing the React application, applying strict mode, and providing the router configuration.
+ */
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
+import Login from './pages/login/Login.jsx'
 
+// Configure the router paths.
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
+// Render the application into the DOM element with id 'root'.
+// The application is wrapped in StrictMode for highlighting potential issues during development.
+// RouterProvider is used to provide the router configuration to the entire application.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
