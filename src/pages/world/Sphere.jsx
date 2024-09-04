a16849d68af399cef529ab9d93324e7410eb9d5c
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2024-09-04 09:15:39 -0500

    documentation

--- src/pages/world/Sphere.jsx
+++ src/pages/world/Sphere.jsx
@@ -1,15 +1,37 @@
+/**
+ * @file Sphere.jsx
+ * @description This file defines the `Sphere` component, which renders a 3D sphere using React Three Fiber.
+ * @date Created: 03/09/2024
+ * @date Last Modified: 04/09/2024
+ * @author Carlos Mauricio Tovar Parra
+ *         carlos.mauricio.tovar@correounivalle.edu.co
+ */
+
 import React from "react";
 
 /* eslint-disable react/no-unknown-property */
-const Sphere = () => {
 
+/**
+ * @component Sphere
+ * @description The `Sphere` component renders a 3D sphere using `@react-three/fiber`. The sphere has a 
+ * standard material with a blue color.
+ * @returns {JSX.Element} A JSX element containing a 3D sphere with specified geometry and material properties.
+ * @example
+ * // Example usage:
+ * <Sphere />
+ */
+const Sphere = () => {
   return (
     <React.Fragment>
-        <mesh name="sphere">
-          <sphereGeometry args={[0.3, 32, 32]} />
-          <meshStandardMaterial color="blue" />
-        </mesh>
+      <mesh name="sphere">
+        {/* Define the geometry of the sphere with a radius of 0.3 and high detail (32 segments). */}
+        <sphereGeometry args={[0.3, 32, 32]} />
+        
+        {/* Apply a standard material with a blue color to the sphere. */}
+        <meshStandardMaterial color="blue" />
+      </mesh>
     </React.Fragment>
   );
 };
+
 export default Sphere;
--- src/pages/world/World.jsx
+++ src/pages/world/World.jsx
@@ -1,3 +1,12 @@
+/**
+ * @file World.jsx
+ * @description This file defines the `World` component, which renders a 3D scene using React Three Fiber and Drei utilities.
+ * @date Created: 03/09/2024
+ * @date Last Modified: 04/09/2024
+ * @author Carlos Mauricio Tovar Parra
+ *         carlos.mauricio.tovar@correounivalle.edu.co
+ */
+
 /* eslint-disable react/no-unknown-property */
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
@@ -5,6 +14,16 @@ import React from "react";
 import Sphere from "./Sphere";
 import "./World.css";
 
+/**
+ * @component World
+ * @description The `World` component renders a 3D scene using React Three Fiber and Drei utilities, 
+ * including a sphere with lighting and interactive camera controls.
+ * @returns {JSX.Element} A JSX element containing a 3D scene with a sphere, ambient light, directional light, 
+ * and orbit controls for interaction.
+ * @example
+ * // Example usage:
+ * <World />
+ */
 const World = () => {
   return (
     <div className="world-container">
